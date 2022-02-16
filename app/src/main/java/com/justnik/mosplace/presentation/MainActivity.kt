a3064e5a4495e09b37052c217466bab0a715c34a
JustNik <justcrazynik@gmail.com>
2022-02-16 20:26:16 +0300

    Initial Commit

---
+++ app/src/main/java/com/justnik/mosplace/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.justnik.mosplace.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.justnik.mosplace.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
