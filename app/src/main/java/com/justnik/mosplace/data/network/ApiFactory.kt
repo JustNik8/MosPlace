baccfb814c44873cb6f0585a11f8d6def3c080d6
JustNik <justcrazynik@gmail.com>
2022-02-16 20:37:41 +0300

    Add district dto

---
+++ app/src/main/java/com/justnik/mosplace/data/network/ApiFactory.kt
@@ -0,0 +1,4 @@
+package com.justnik.mosplace.data.network
+
+object ApiFactory {
+}
---
+++ app/src/main/java/com/justnik/mosplace/data/network/model/DistrictDto.kt
@@ -0,0 +1,14 @@
+package com.justnik.mosplace.data.network.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class DistrictDto (
+    @SerializedName("id")
+    @Expose
+    val id: Int,
+
+    @SerializedName("title")
+    @Expose
+    val title: String
+)
