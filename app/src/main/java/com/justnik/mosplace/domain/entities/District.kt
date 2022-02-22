37e0d240d0aee3fa6eec0ceea50ef1d2de425e20
JustNik <justcrazynik@gmail.com>
2022-02-22 17:26:14 +0300

    add photo from api

--- app/src/main/java/com/justnik/mosplace/domain/entities/District.kt
+++ app/src/main/java/com/justnik/mosplace/domain/entities/District.kt
@@ -1,10 +1,14 @@
 package com.justnik.mosplace.domain.entities
 
 import android.os.Parcelable
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class District (
     val id: Int,
-    val title: String
+    val title: String,
+    val abbreviation: String,
+    val imageUrl: String?
 ) : Parcelable
