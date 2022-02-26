d00e365991060aeafc98320336f707d5ccf0583a
JustNik <justcrazynik@gmail.com>
2022-02-26 16:47:29 +0300

    Add bottom navigation to main activity and observe item click

--- app/src/main/java/com/justnik/mosplace/presentation/MainActivity.kt
+++ app/src/main/java/com/justnik/mosplace/presentation/MainActivity.kt
@@ -1,30 +1,38 @@
 package com.justnik.mosplace.presentation
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.justnik.mosplace.R
+import com.justnik.mosplace.databinding.ActivityMainBinding
 import com.justnik.mosplace.domain.entities.District
 import com.justnik.mosplace.domain.entities.Place
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 
 class MainActivity : AppCompatActivity() {
-    private val scope = CoroutineScope(Dispatchers.Default)
-
-    private val viewModel: MosViewModel by lazy {
-        ViewModelProvider(this)[MosViewModel::class.java]
-    }
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        launchDistrictsFragment()
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        observeBottomNavigation()
+        binding.mainBottomNavigation.selectedItemId = R.id.page_main
     }
 
-    private fun launchDistrictsFragment(){
+    private fun observeBottomNavigation() {
+        binding.mainBottomNavigation.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.page_main -> launchDistrictsFragment()
+            }
+            true
+        }
+    }
+
+    private fun launchDistrictsFragment() {
         val fragment = DistrictsFragment.newInstance()
         fragment.onDistrictCLickListener = {
             launchDistrictPlacesFragment(it)
@@ -35,7 +43,7 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
-    private fun launchDistrictPlacesFragment(district: District){
+    private fun launchDistrictPlacesFragment(district: District) {
         val fragment = DistrictPlacesFragment.newInstance(district)
 
         fragment.onPlaceClickListener = {
@@ -48,7 +56,7 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
-    private fun launchPlaceFragment(place: Place){
+    private fun launchPlaceFragment(place: Place) {
         val fragment = PlaceFragment.newInstance(place)
 
         supportFragmentManager.beginTransaction()
