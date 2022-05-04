5ee0f24593f1ac72d29398592b0624ba8b081e77
JustNik <justcrazynik@gmail.com>
2022-05-04 20:19:13 +0300

    ask location permissions and show user location

--- app/src/main/java/com/justnik/mosplace/presentation/map/MapFragment.kt
+++ app/src/main/java/com/justnik/mosplace/presentation/map/MapFragment.kt
@@ -1,18 +1,17 @@
 package com.justnik.mosplace.presentation.map
 
+import android.graphics.Color
+import android.graphics.PointF
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.*
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.justnik.mosplace.R
-import com.justnik.mosplace.data.mock.MockData
 import com.justnik.mosplace.data.network.PlaceTypes
 import com.justnik.mosplace.databinding.FragmentMapBinding
 import com.justnik.mosplace.domain.entities.Place
@@ -21,22 +20,31 @@ import com.justnik.mosplace.helpers.observeFlow
 import com.justnik.mosplace.helpers.showSupportActionBar
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
-import com.yandex.mapkit.map.CameraPosition
-import com.yandex.mapkit.map.MapObjectTapListener
+import com.yandex.mapkit.layers.ObjectEvent
+import com.yandex.mapkit.map.*
+import com.yandex.mapkit.user_location.UserLocationLayer
+import com.yandex.mapkit.user_location.UserLocationObjectListener
+import com.yandex.mapkit.user_location.UserLocationView
+import com.yandex.runtime.image.ImageProvider
 import com.yandex.runtime.ui_view.ViewProvider
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import java.util.jar.Manifest
 
 @AndroidEntryPoint
-class MapFragment : Fragment(R.layout.fragment_map) {
+class MapFragment : Fragment(R.layout.fragment_map), UserLocationObjectListener {
 
     private val binding: FragmentMapBinding by viewBinding()
     private val viewModel: MapViewModel by viewModels()
 
+    private val locationPermissionRequestLauncher = registerForActivityResult(
+        RequestMultiplePermissions(),
+        this::onGotPermissionsLocationResult
+    )
+
     private val mapView by lazy { binding.mapView }
     private val mapObjects by lazy { mapView.map.mapObjects.addCollection() }
+
+    private lateinit var userLocationLayer: UserLocationLayer
 
     private val tapListener = MapObjectTapListener { mapObject, point ->
         val place = mapObject.userData as Place
@@ -46,12 +54,22 @@ class MapFragment : Fragment(R.layout.fragment_map) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MapKitFactory.initialize(requireContext())
         hideSupportActionBar()
+        MapKitFactory.initialize(requireContext())
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setUpMapView()
+        checkLocationPermissions()
+    }
+
+    private fun checkLocationPermissions() {
+        val coarseLocationPermission = android.Manifest.permission.ACCESS_COARSE_LOCATION
+        val fineLocationPermission = android.Manifest.permission.ACCESS_FINE_LOCATION
+
+        locationPermissionRequestLauncher.launch(
+            arrayOf(coarseLocationPermission, fineLocationPermission)
+        )
     }
 
     override fun onStart() {
@@ -79,9 +97,23 @@ class MapFragment : Fragment(R.layout.fragment_map) {
         addPlacemarks()
     }
 
+    private fun onGotPermissionsLocationResult(grantResult: Map<String, Boolean>){
+        if (grantResult.entries.all { it.value }){
+            setupUserLocationLayer()
+        }
+    }
+
+    private fun setupUserLocationLayer(){
+        val mapKit = MapKitFactory.getInstance()
+        userLocationLayer = mapKit.createUserLocationLayer(mapView.mapWindow)
+        userLocationLayer.isVisible = true
+        userLocationLayer.isHeadingEnabled = true
+        userLocationLayer.setObjectListener(this)
+    }
+
     private fun addPlacemarks() {
-        viewModel.uiState.observeFlow(viewLifecycleOwner){ uiState ->
-            uiState.places.forEach{ place ->
+        viewModel.uiState.observeFlow(viewLifecycleOwner) { uiState ->
+            uiState.places.forEach { place ->
                 val point = Point(place.latitude, place.longitude)
                 val viewProvider = ViewProvider(getImageViewByPlaceType(place.type))
                 val mark = mapObjects.addPlacemark(point, viewProvider)
@@ -109,5 +141,47 @@ class MapFragment : Fragment(R.layout.fragment_map) {
 
     companion object {
         private val MOSCOW_LOCATION_POINT = Point(55.753789, 37.6209230)
+    }
+
+    override fun onObjectAdded(userLocationView: UserLocationView) {
+        userLocationLayer.setAnchor(
+            PointF((mapView.width * 0.5).toFloat(), (mapView.height * 0.5).toFloat()),
+            PointF((mapView.width * 0.5).toFloat(), (mapView.height * 0.83).toFloat())
+        )
+
+        userLocationView.arrow.setIcon(
+            ImageProvider.fromResource(
+                requireContext(), R.drawable.ic_arrow_back
+            )
+        )
+
+        val pinIcon: CompositeIcon = userLocationView.pin.useCompositeIcon()
+
+        pinIcon.setIcon(
+            "icon",
+            ImageProvider.fromResource(requireContext(), R.drawable.ic_account),
+            IconStyle().setAnchor(PointF(0f, 0f))
+                .setRotationType(RotationType.ROTATE)
+                .setZIndex(0f)
+                .setScale(1f)
+        )
+
+        pinIcon.setIcon(
+            "pin",
+            ImageProvider.fromResource(requireContext(), R.drawable.ic_check),
+            IconStyle().setAnchor(PointF(0.5f, 0.5f))
+                .setRotationType(RotationType.ROTATE)
+                .setZIndex(1f)
+                .setScale(0.5f)
+        )
+
+        userLocationView.accuracyCircle.fillColor = Color.BLUE and -0x66000001
+    }
+
+    override fun onObjectRemoved(p0: UserLocationView) {
+
+    }
+
+    override fun onObjectUpdated(p0: UserLocationView, p1: ObjectEvent) {
     }
 }
